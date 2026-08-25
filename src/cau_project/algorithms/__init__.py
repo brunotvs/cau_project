5646121bc4a337bf19e4939932baf117e642f80d
Bruno Thiago <brunothiagorvs@gmail.com>
2026-08-25 18:17:52 -0300

    feat: exporting new algorithms

--- src/cau_project/algorithms/__init__.py
+++ src/cau_project/algorithms/__init__.py
@@ -1,7 +1,17 @@
+from .albedo import albedo
 from .bh import building_heights
 from .dem import dem
 from .dsm import dsm
 from .ndvi import ndvi
 from .svf import svf
+from .ruggedness import ruggedness
 
-__all__ = ["building_heights", "dem", "dsm", "ndvi", "svf"]
+__all__ = [
+    "albedo",
+    "building_heights",
+    "dem",
+    "dsm",
+    "ndvi",
+    "ruggedness",
+    "svf",
+]
