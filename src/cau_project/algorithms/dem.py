22f2d540ce93be9f1d626a68c516e065c3caf6ff
Bruno Thiago <brunothiagorvs@gmail.com>
2026-08-25 18:17:00 -0300

    fix: dem reprojection

--- src/cau_project/algorithms/dem.py
+++ src/cau_project/algorithms/dem.py
@@ -20,9 +20,9 @@ def _builder(
     def dem(img: ee.Image = empty_image):
         dem = (
             ee.Image("USGS/SRTMGL1_003")
+            .convolve(ee.Kernel.gaussian(radius=1.5, sigma=1, units="pixels"))
             .rename(output_band)
             .resample("bicubic")
-            .reproject(crs="EPSG:4326", scale=2)
         )
 
         return img.addBands(dem)
