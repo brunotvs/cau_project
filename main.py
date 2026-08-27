bcee53110835481bedf5df7a62697c81b5e4a347
Bruno Thiago <brunothiagorvs@gmail.com>
2026-08-27 15:37:20 -0300

    feat: code cleanup

--- main.py
+++ main.py
@@ -18,11 +18,6 @@ region = (
     .filter(ee.Filter.eq("ADM2_NAME", "Rio Grande Da Serra"))
 )
 
-# region = ee.FeatureCollection(
-#     [ee.Geometry.Point([-46.398372, -23.752964]).buffer(150).bounds()]
-#     # [ee.Geometry.Point([-46.64488467700909, -23.641405219380577]).buffer(150).bounds()]
-# )
-
 Map.add_layer(
     ee_object=region.style(fillColor="0000", color="000F", width=5.0),
     vis_params={},
@@ -36,56 +31,8 @@ Map.center_object(region)
     ee.ImageCollection([ee.Image(1).clip(region.geometry())])
     .filterBounds(region)
     .map(cau_algorithms.dem())
-    .aside(
-        cau_map.add_layer_to_map(
-            {
-                "band": "dem",
-                "min_max_strategy": cau_map.percentile_min_max(region.geometry()),
-                "palette": [
-                    "#0000FF",
-                    "#000080",
-                    "#00FFFF",
-                    "#FFFF00",
-                    "#FF0000",
-                ],
-            }
-        ),
-        Map,
-    )
     .map(cau_algorithms.building_heights())
-    .aside(
-        cau_map.add_layer_to_map(
-            {
-                "band": "bh",
-                "min_max_strategy": cau_map.absolute_min_max(region.geometry()),
-                "palette": [
-                    "#000080",
-                    "#0000FF",
-                    "#00FFFF",
-                    "#FFFF00",
-                    "#FF0000",
-                ],
-            }
-        ),
-        Map,
-    )
     .map(cau_algorithms.dsm())
-    .aside(
-        cau_map.add_layer_to_map(
-            {
-                "band": "dsm",
-                "min_max_strategy": cau_map.absolute_min_max(region.geometry()),
-                "palette": [
-                    "#000080",
-                    "#0000FF",
-                    "#00FFFF",
-                    "#FFFF00",
-                    "#FF0000",
-                ],
-            }
-        ),
-        Map,
-    )
     .map(
         cau_algorithms.svf(
             {"dsm_band": "dsm", "num_directions": 64, "num_elevations": 64 // 4}
@@ -121,46 +68,13 @@ Map.center_object(region)
         ),
         Map,
     )
-    .map(cau_algorithms.ruggedness({"dem_band": "dsm"}))
-    .aside(
-        cau_map.add_layer_to_map(
-            {
-                "band": "ruggedness",
-                "min_max_strategy": cau_map.percentile_min_max(region.geometry()),
-                "palette": [
-                    "purple",
-                    "blue",
-                    "green",
-                    "yellow",
-                    "red",
-                    "black",
-                ],
-            }
-        ),
-        Map,
+    .map(
+        cau_algorithms.ruggedness(
+            {"height_band": "dsm", "radius": 45, "radius_units": "meters"}
+        )
     )
     .map(cau_algorithms.lst())
-    .aside(
-        cau_map.add_layer_to_map(
-            {
-                "band": "lst",
-                "min_max_strategy": cau_map.absolute_min_max(region.geometry()),
-                "palette": [
-                    "#000004",
-                    "#1b0c41",
-                    "#4a0c6b",
-                    "#781c6d",
-                    "#a52c60",
-                    "#cf4446",
-                    "#ed6925",
-                    "#fb9b06",
-                    "#f7d13c",
-                    "#fcffa4",
-                ],
-            }
-        ),
-        Map,
-    )
+    .map(cau_algorithms.compactness({"radius": 100, "radius_units": "meters"}))
     .mosaic()
     .clip(region)
 )
